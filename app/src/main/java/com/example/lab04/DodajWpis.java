80dca5a4e7a7d97c2ffba8f88244e8226557f65d
Ireneusz Nowak <ni39380@zut.edu.pl>
2019-05-20 11:52:11 +0200

    Initial commit

---
+++ app/src/main/java/com/example/lab04/DodajWpis.java
@@ -0,0 +1,29 @@
+package com.example.lab04;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
+
+public class DodajWpis extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dodaj_wpis);
+    }
+    public void wyslij (View view)
+    {
+        EditText kontrolka =
+                (EditText)findViewById(R.id.editText);
+        String pole =
+                kontrolka.getText().toString();
+        Intent intencja = new Intent();
+        intencja.putExtra("wpis", pole);
+        setResult(RESULT_OK, intencja);
+        finish();
+
+    }
+}
